9e68e48e4dd7db0616b704e890dac10d8558249d
Victor Iliya <VictorIliya15@gmail.com>
2024-12-09 13:48:47 +0100

    Created the internet gateway and its acttachment for the vpc

--- Terraform/infrastructure/vpc/vpc.tf
+++ Terraform/infrastructure/vpc/vpc.tf
@@ -1,3 +1,4 @@
+#VPC
 resource "aws_vpc" "three-tier_vpc" {
   cidr_block           = "10.2.0.0/16"
   enable_dns_support   = true
@@ -6,4 +7,17 @@ resource "aws_vpc" "three-tier_vpc" {
   tags = {
     name = "three-tier-vpc"
   }
+}
+#interget gateway to give vpc internet access
+resource "aws_internet_gateway" "three-tier_igw" {
+  vpc_id = aws_vpc.three-tier_vpc.id
+
+  tags = {
+    name = "three-tier-igw"
+  }
+}
+
+resource "aws_internet_gateway_attachment" "three-tier_igw" {
+  internet_gateway_id = aws_internet_gateway.three-tier_igw.id
+  vpc_id              = aws_vpc.three-tier_vpc.id
 }
